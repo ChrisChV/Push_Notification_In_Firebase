0a49bc7acdd2d0ad5b9809fdfd63824d33bab96b
ChrisChV <cristo2014f@outlook.es>
2018-11-05 17:10:20 -0500

    finish

--- httpPost.sh
+++ httpPost.sh
@@ -1,2 +1,2 @@
 
-curl -H 'Content-Type:application/json' -H 'Authorization:key=AAAA3c_98Kw:APA91bEBeK3FwbUEBT5sQR_qaKHUbwfHXUfFMgPocmyMZbwe1NL8RRrMnu2iupBclywrLdfAlk8b3794aGZEtV7sSqRhhKsjmZlOGOUjXdcj-jwmBiqk7uWFWbFz3J7zIhf3FfOi69eM' -d '{"to": "/topics/test", "data": {"title":"Test","message": "This is a Firebase Cloud Messaging Topic Message!", }}' https://fcm.googleapis.com/fcm/send
+curl -H 'Content-Type:application/json' -H 'Authorization:key=AAAAA:TU-KEY' -d '{"to": "/topics/test", "data": {"title":"Test","message": "This is a Firebase Cloud Messaging Topic Message!", }}' https://fcm.googleapis.com/fcm/send
